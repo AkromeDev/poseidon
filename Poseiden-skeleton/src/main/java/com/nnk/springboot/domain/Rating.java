1efdb85fcf9d51b8de05ed58f35b241d823398f8
jcbackenddev <jcbackenddev@gmail.com>
2021-02-26 09:08:48 +0100

    added getters, setters, toString and test constructor

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -8,5 +8,80 @@ import java.sql.Timestamp;
 @Entity
 @Table(name = "rating")
 public class Rating {
-    // TODO: Map columns in data table RATING with corresponding java fields
+
+	@Column(name = "id")
+	Integer id;
+	
+	@Column(name = "moodysRating")
+	String moodysRating;
+	
+	@Column(name = "sandPRating")
+	String sandPRating;
+	
+	@Column(name = "fitchRating")
+	String fitchRating;
+	
+	@Column(name = "orderNumber")
+	Integer orderNumber;
+
+	/***
+	 * Constructor for test purposes
+	 * @param moodysRating
+	 * @param sandPRating
+	 * @param fitchRating
+	 * @param orderNumber
+	 */
+	public Rating(String moodysRating, String sandPRating, String fitchRating, int orderNumber) {
+		this.moodysRating = moodysRating;
+		this.sandPRating = sandPRating;
+		this.fitchRating = fitchRating;
+		this.orderNumber = orderNumber;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getMoodysRating() {
+		return moodysRating;
+	}
+
+	public void setMoodysRating(String moodysRating) {
+		this.moodysRating = moodysRating;
+	}
+
+	public String getSandPRating() {
+		return sandPRating;
+	}
+
+	public void setSandPRating(String sandPRating) {
+		this.sandPRating = sandPRating;
+	}
+
+	public String getFitchRating() {
+		return fitchRating;
+	}
+
+	public void setFitchRating(String fitchRating) {
+		this.fitchRating = fitchRating;
+	}
+
+	public Integer getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(Integer orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Rating [id=" + id + ", moodysRating=" + moodysRating + ", sandPRating=" + sandPRating + ", fitchRating="
+				+ fitchRating + ", orderNumber=" + orderNumber + "]";
+	}
+	
 }
