bd8fca80d0a2f651286902ae9e56b6958a7819c7
jcbackenddev <jcbackenddev@gmail.com>
2021-02-26 09:00:19 +0100

    Added getter, setters, toString and constructor to curvePoint entity.

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -11,5 +11,85 @@ import java.sql.Timestamp;
 @Entity
 @Table(name = "curvepoint")
 public class CurvePoint {
-    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+    
+	@Column(name = "id")
+	Integer id;
+	
+	@Column(name = "curveId")
+	Integer curveId;
+	
+	@Column(name = "asOfDate")
+	Timestamp asOfDate;
+	
+	@Column(name = "term")
+	Double term;
+	
+	@Column(name = "value")
+	Double value;
+	
+	@Column(name = "creationDate")
+	Timestamp creationDate;
+
+	public CurvePoint(int curveId, double term, double value) {
+		this.curveId = curveId;
+		this.term = term;
+		this.value = value;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getCurveId() {
+		return curveId;
+	}
+
+	public void setCurveId(Integer curveId) {
+		this.curveId = curveId;
+	}
+
+	public Timestamp getAsOfDate() {
+		return asOfDate;
+	}
+
+	public void setAsOfDate(Timestamp asOfDate) {
+		this.asOfDate = asOfDate;
+	}
+
+	public Double getTerm() {
+		return term;
+	}
+
+	public void setTerm(Double term) {
+		this.term = term;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	public Timestamp getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Timestamp creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	@Override
+	public String toString() {
+		return "CurvePoint [id=" + id + ", curveId=" + curveId + ", asOfDate=" + asOfDate + ", term=" + term
+				+ ", value=" + value + ", creationDate=" + creationDate + "]";
+	}
+	
+	
+	
 }
