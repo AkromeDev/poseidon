9e8c7bae096fbeefa86251325357eae2991f0036
jcbackenddev <jcbackenddev@gmail.com>
2021-03-05 10:14:06 +0100

    Changed logs.

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/service/BidListService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/BidListService.java
@@ -2,19 +2,26 @@ package com.nnk.springboot.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nnk.springboot.controllers.BidListController;
 import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.repositories.BidListRepository;
 
 @Service
 public class BidListService {
+	
+	private static final Logger log = LoggerFactory.getLogger(BidListService.class);
 
 	@Autowired
 	BidListRepository bidRepo;
 	
 	public BidList saveBid(BidList bid) {
+		log.info("SAVING..." + bid);
+		
 		return bidRepo.save(bid);
 	}
 
