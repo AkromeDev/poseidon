cd694c5707a3a768246353496d691e1166853bd1
jcbackenddev <jcbackenddev@gmail.com>
2021-03-05 10:07:31 +0100

    Created Service classes.
    They are not implemented as yet!

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/RuleNameService.java
@@ -0,0 +1,32 @@
+package com.nnk.springboot.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nnk.springboot.controllers.BidListController;
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.repositories.BidListRepository;
+
+@Service
+public class RuleNameService {
+	
+	private static final Logger log = LoggerFactory.getLogger(RuleNameService.class);
+
+	@Autowired
+	BidListRepository bidRepo;
+	
+	public BidList saveBid(BidList bid) {
+		log.info("SAVING..." + bid);
+		
+		return bidRepo.save(bid);
+	}
+
+	public List<BidList> findAll() {
+		return bidRepo.findAll();
+	}
+	
+}
